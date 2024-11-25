4a11acd91dcda792b697356e0d24d8dd095340c8
Paozindimel <68926794+Paozindimel@users.noreply.github.com>
2024-11-25 15:42:15 -0300

    Add files via upload

---
+++ perguntas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Gaby',
+    'O que a gaby não gosta de comer?',
+    'Tomate, carne de porco, frango, arroz doce, e carne moida'
+)
+
+criaCartao(
+    'Jogos',
+    'Sextou com c de?',
+    'Cs'
+)
+
+criaCartao(
+    'gato',
+    'Uma gata que gosta de chinelo...',
+    'Ravena'
+)
+
+criaCartao(
+    'Lugar',
+    'Para onde vai as coisas que some?',
+    'Cucuias'
+)
